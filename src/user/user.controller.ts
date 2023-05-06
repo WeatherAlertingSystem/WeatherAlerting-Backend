6b94c78c73470ae80ff726b7c51caee67cf63064
Piotr Bednarek <piotrbednarek99@gmail.com>
2023-05-06 22:23:17 +0200

    Implement /user/create-trigger endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,17 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
-import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { User } from './schema/user.schema';
-import { WeatherTriggerService } from 'src/weather-trigger/weather-trigger.service';
 import { CreateWeatherTriggerDto } from 'src/weather-trigger/dto/create-weather-trigger.dto';
 import { WeatherTrigger } from 'src/weather-trigger/schema/weather-trigger.schema';
+import { WeatherTriggerService } from 'src/weather-trigger/weather-trigger.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './schema/user.schema';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(
     private userService: UserService,
     private weatherTriggerService: WeatherTriggerService,
-  ) {}
+  ) { }
   @Get()
   async findAll(): Promise<User[]> {
     return this.userService.findAll();
@@ -22,14 +22,12 @@ export class UserController {
     return this.userService.create(createUserDto);
   }
 
-  // @Post('create-trigger')
-  // async createTrigger(
-  //   @Body() weatherTriggerDto: CreateWeatherTriggerDto,
-  // ): Promise<WeatherTrigger> {
-  //   //Create new Trigger object
-  //   const newID = this.weatherTriggerService.create(weatherTriggerDto);
-  //   // Add ID od new Trigger object to users subscription list
-  //   this.userService.addSubscriptionToUser()
-  //   return newID;
-  // }
+  @Post('create-trigger')
+  async createTrigger(
+    @Body() weatherTriggerDto: CreateWeatherTriggerDto,
+  ): Promise<WeatherTrigger> {
+    const newID = await this.weatherTriggerService.create(weatherTriggerDto);
+    this.userService.addSubscriptionToUser(newID, '6456b288539862e332eba0bd');
+    return newID;
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { User } from './schema/user.schema';
 import { Model } from 'mongoose';
+import { WeatherTrigger } from 'src/weather-trigger/schema/weather-trigger.schema';
 import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './schema/user.schema';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  constructor(@InjectModel(User.name) private userModel: Model<User>) { }
 
   async create(createUserDto: CreateUserDto): Promise<User> {
     // TODO: Hash the password
@@ -18,11 +19,12 @@ export class UserService {
     return this.userModel.find().exec();
   }
 
-  // async addSubscriptionToUser(
-  //   subscriptionID: number,
-  //   userID: string,
-  // ): Promise<void> {
-  //   const myUser = await this.userModel.findById(userID).exec();
-  //   myUser.subscriptions.push(subscriptionID);
-  // }
+  async addSubscriptionToUser(
+    subscriptionID: WeatherTrigger,
+    userID: string, //TODO: In future we should detect user in a better way than string with ID!
+  ): Promise<void> {
+    const myUser = await this.userModel.findById(userID).exec();
+    myUser.subscriptions.push(subscriptionID);
+    myUser.save();
+  }
 }
